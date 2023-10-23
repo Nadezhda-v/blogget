3f230ad0bdf1ab189d1775b478f02ff3ab02c41f
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-10-23 14:56:50 +0400

    Added a drop-down menu and auxiliary components

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,17 +2,38 @@ import style from './List.module.css';
 import Post from './Post';
 
 export const List = () => {
-  const postData = {
-    preview: '',
-    title: 'Title',
-    author: 'Nickname',
-    ups: 10,
-    date: '2023-10-19T19:08:00.000Z'
-  };
+  const postsData = [
+    {
+      preview: '',
+      title: 'Title1',
+      author: 'Nickname1',
+      ups: 10,
+      date: '2023-10-19T19:08:00.000Z',
+      id: 78855,
+    },
+    {
+      preview: '',
+      title: 'Title2',
+      author: 'Nickname2',
+      ups: 15,
+      date: '2023-10-20T15:01:00.000Z',
+      id: 12428,
+    },
+    {
+      preview: '',
+      title: 'Title3',
+      author: 'Nickname3',
+      ups: 20,
+      date: '2023-10-18T09:18:00.000Z',
+      id: 75825,
+    },
+  ];
 
   return (
-    <ul className={style.list}>
-      <Post postData={postData}/>
+    <ul className={style.list}>{
+      postsData.map(postData => (
+        <Post key={postData.id} postData={postData} />
+      ))}
     </ul>
   );
 };
